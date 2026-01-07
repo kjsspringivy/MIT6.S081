e9ef4d339776181d73865bcfacc52f3fbb36975b
kjs <898325455@qq.com>
2026-01-07 20:25:37 +0800

    Speed up system calls

--- kernel/elf.h
+++ kernel/elf.h
@@ -10,16 +10,17 @@ struct elfhdr {
   ushort machine;  // 目标机器架构
   uint version;  // ELF文件版本
   uint64 entry;  // 程序入口地址, 程序开始执行的第一条指令的虚拟地址
+  uint64 phoff;  // 程序头表在文件中的起始偏移量
+  uint64 shoff;  // 节头表在文件中的偏移量
   uint flags;  // 与处理器相关的标志
   ushort ehsize;  // ELF文件头的大小
-  uint64 phoff;  // 程序头表在文件中的起始偏移量
   ushort phentsize;  // 一个程序头表的的大小
   ushort phnum;  // 程序头表中条目的数量
-  uint64 shoff;  // 节头表在文件中的偏移量
   ushort shentsize;  // 一个节头表的大小
   ushort shnum;  // 节头表中条目的数量
   ushort shstrndx;  // 节头表中保存节名称的节的索引
 };
+
 
 // Program section header
 struct proghdr {
