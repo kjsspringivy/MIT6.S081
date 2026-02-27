e0fbd6dc39dfef2b3e82c62661cf9ea77f226c70
jinshankong <898325455@qq.com>
2026-02-27 14:30:26 +0800

    Barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -8,8 +8,8 @@ static int nthread = 1;
 static int round = 0;
 
 struct barrier {
-  pthread_mutex_t barrier_mutex;
-  pthread_cond_t barrier_cond;
+  pthread_mutex_t barrier_mutex;  // 锁
+  pthread_cond_t barrier_cond;  // 条件
   int nthread;      // Number of threads that have reached this round of the barrier
   int round;     // Barrier round
 } bstate;
@@ -26,11 +26,20 @@ static void
 barrier()
 {
   // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+  // Block until all threads have called barrier() and then increment bstate.round.
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  if(bstate.nthread == nthread){
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  } else {
+    int my_round = bstate.round;
+    while(bstate.round == my_round){
+      pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
